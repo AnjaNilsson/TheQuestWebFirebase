c8f694b4a7b1f5d70370a525e6266c426d2043ae
Niclas Lund Stisager <nls@netcompany.com>
2019-05-01 19:52:39 +0100

    Fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // Initialize Firebase
 var config = {
-
+    apiKey: "AIzaSyDKtbbK0x-MUqXupFZkDBbFxzPlU9ArBNQ",
+    authDomain: "the-quest-3.firebaseapp.com",
+    databaseURL: "https://the-quest-3.firebaseio.com",
+    projectId: "the-quest-3",
+    storageBucket: "the-quest-3.appspot.com",
+    messagingSenderId: "216176601003"
 };
 firebase.initializeApp(config);
 
