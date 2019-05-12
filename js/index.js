cb9a93e36e827029d3f601c009ba91ded5315fd3
Niclas Lund Stisager <nls@netcompany.com>
2019-05-12 20:15:28 +0100

    Parse game details to Firestore

--- js/index.js
+++ js/index.js
@@ -9,42 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var minigameCards = "";
-
 var firestore = firebase.firestore();
-
-firestore.collection("minigames").get().then(function(querySnapshot) {
-
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        minigameCards += '<div id="'+doc.id+'" class="card custom-card mx-2 mb-3">'
-        + '<div class="card-body">'
-        + '<h4 class="card-title">'+doc.data().name+'</h4>'
-        + '<p class="card-text p-y-1">'+doc.data().description+'</p>'
-        + '<label class="switch">'
-        + '<input type="checkbox" id="check_'+doc.id+'">'
-        + '<span for="check_'+doc.id+'" class="slider"></span>'
-        + '</label>'
-        + '<input type="number" class="form-control" id="lat_'+doc.id+'" placeholder="Latitude">'
-        + '<input type="number" class="form-control" id="lon_'+doc.id+'" placeholder="Longitude">'
-        + '</div>'
-        + '</div>'
-
-    });
-    $('#minigame-card').html(minigameCards);
-});
-
-
-function createNewGame() {
-    var gameId = getGameId();
-    
-    firestore.collection("games").doc(gameId).set({
-        config: []
-    });
-    
-    return "New game " + gameId + " created!"; 
-};
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -74,6 +39,51 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 
+firestore.collection("minigames").get().then(function(querySnapshot) {
+    var minigameCards = "";
+
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        minigameCards += '<div id="'+doc.id+'" class="card custom-card mx-2 mb-3">'
+        + '<div class="card-body">'
+        + '<h4 class="card-title">'+doc.data().name+'</h4>'
+        + '<p class="card-text p-y-1">'+doc.data().description+'</p>'
+        + '<label class="switch">'
+        + '<input type="checkbox" id="check_'+doc.id+'">'
+        + '<span for="check_'+doc.id+'" class="slider"></span>'
+        + '</label>'
+        + '<input type="number" class="form-control" id="lat_'+doc.id+'" placeholder="Latitude">'
+        + '<input type="number" class="form-control" id="lon_'+doc.id+'" placeholder="Longitude">'
+        + '</div>'
+        + '</div>'
+
+    });
+    $('#minigame-card').html(minigameCards);
+});
+
+
+function createNewGame() {
+    var gameId = getGameId();
+    var selectedGames = [];
+    var user = firebase.auth().currentUser.uid;
+    
+    var selectedGames = getSelected();
+
+    firestore.collection("games").doc(gameId).set({
+        config: selectedGames,
+        user: user
+    })
+    .then(function() {
+        console.log("Document written with ID: ", gameId, selectedGames);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+
+};
+
+
 function login() {
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
@@ -92,6 +102,21 @@ function logout() {
     firebase.auth().signOut();
 }
 
+function getSelected() {
+    var selected = [];
+    $('.switch input:checked').each(function() {
+        var selectedId = $(this).attr('id').substr(6);
+        
+        selected.push({
+            [selectedId]: {
+                "lat": $('#lat_' + selectedId).val(),
+                "lon": $('#lon_' + selectedId).val()
+            }
+        });
+
+    });
+    return selected;
+}
 
 function getGameId() {
     var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZ";
