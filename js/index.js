d8c4c9ddafb7a878322237d4c247e33d63900986
Niclas Lund Stisager <nls@netcompany.com>
2019-05-12 17:04:50 +0100

    Create game in Firestore with config

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDKtbbK0x-MUqXupFZkDBbFxzPlU9ArBNQ",
@@ -20,41 +18,33 @@ firestore.collection("minigames").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data());
-        minigameCards += '<div class="col-md-4">'
-        + '<div class="card">'
-        + '<div class="card-block">'
+        minigameCards += '<div id="'+doc.id+'" class="card custom-card mx-2 mb-3">'
+        + '<div class="card-body">'
         + '<h4 class="card-title">'+doc.data().name+'</h4>'
         + '<p class="card-text p-y-1">'+doc.data().description+'</p>'
-        + '<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">'
-        + '<label class="form-check-label" for="defaultCheck1"> </label>'
+        + '<label class="switch">'
+        + '<input type="checkbox" id="check_'+doc.id+'">'
+        + '<span for="check_'+doc.id+'" class="slider"></span>'
+        + '</label>'
+        + '<input type="number" class="form-control" id="lat_'+doc.id+'" placeholder="Latitude">'
+        + '<input type="number" class="form-control" id="lon_'+doc.id+'" placeholder="Longitude">'
         + '</div>'
         + '</div>'
-        + '</div>'
+
     });
     $('#minigame-card').html(minigameCards);
 });
 
-//TRYING TO ADD TO THE DATABSE -- NOT WORKING!!
-/* var firestore = firebase.firestore();
 
-const docRef = firestore.doc("samples/sandwichData");
-const outputHeader = document.querySelector("#hotDogOutput");
-const inputTextField = document.querySelector("#latestHodDogStatus");
-const saveButton = document.querySelector("#saveButton");
-
-
-saveButton.addEventListener("click", function () {
-    const textToSave = inputTextField.value;
-    console.log("I am going to save " + textToSave + " to Firestore");
-    docRef.set({
-        hotDogStatus: textToSave
-    }).then(function() {
-        console.log("Status Saved");
-     }).catch(function (error) {
-        console.log("Got an error: ", error);
+function createNewGame() {
+    var gameId = getGameId();
+    
+    firestore.collection("games").doc(gameId).set({
+        config: []
     });
-})
- */
+    
+    return "New game " + gameId + " created!"; 
+};
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -85,7 +75,6 @@ firebase.auth().onAuthStateChanged(function (user) {
 });
 
 function login() {
-
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
@@ -104,8 +93,7 @@ function logout() {
 }
 
 
-function randomString() {
-    //var userRandomNumber = document.getElementById("randomnumber_field").value;
+function getGameId() {
     var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZ";
     var string_length = 4;
     var randomstring = '';
@@ -113,9 +101,7 @@ function randomString() {
         var rnum = Math.floor(Math.random() * chars.length);
         randomstring += chars.substring(rnum, rnum + 1);
     }
-    document.randform.randomfield.value = randomstring;
+
+    return randomstring;
 }
-
-});
-
 
